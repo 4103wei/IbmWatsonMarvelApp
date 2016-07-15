de49bac311dd8dd43563391ce05003953390886e
Wei <jerryjerry4103@gmail.com>
2016-07-16 01:55:02 +0200

    user interface for the quiz

--- app/whoami.component.js
+++ app/whoami.component.js
@@ -12,6 +12,10 @@ var core_1 = require('@angular/core');
 var WhoAmIComponent = (function () {
     function WhoAmIComponent() {
     }
+    WhoAmIComponent.prototype.startTimer = function () {
+        document.getElementById("gamewindow").innerHTML = "<div id ='delay'>GET READY...</div>";
+        setTimeout(initQuestions, 2000);
+    };
     WhoAmIComponent = __decorate([
         core_1.Component({
             selector: 'home',
@@ -22,4 +26,30 @@ var WhoAmIComponent = (function () {
     return WhoAmIComponent;
 }());
 exports.WhoAmIComponent = WhoAmIComponent;
+function initQuestions() {
+    //TODO: Generate Question
+    var question = 'What is the real name of Black Widow?';
+    //TODO: Fetch 4 answers from Watson
+    var answers = ['Peter Parker', 'Natalia Alianovna', 'Anthony Edward Stark', 'Wanda Maximoff'];
+    document.getElementById("gamewindow").innerHTML = "<center><div id=question></div><br>\
+        <button class='button' id='ans1'>Answer 1</button>\
+        <button class='button' id='ans2'>Answer 2</button>\
+        <button class='button' id='ans3'>Answer 3</button>\
+        <button class='button' id='ans4'>Answer 4</button></center>";
+    document.getElementById("ans1").onclick = function () { answerhandler(1); };
+    document.getElementById("ans2").onclick = function () { answerhandler(2); };
+    document.getElementById("ans3").onclick = function () { answerhandler(3); };
+    document.getElementById("ans4").onclick = function () { answerhandler(4); };
+    document.getElementById("question").innerHTML = question;
+    document.getElementById("ans1").innerHTML = answers[0];
+    document.getElementById("ans2").innerHTML = answers[1];
+    document.getElementById("ans3").innerHTML = answers[2];
+    document.getElementById("ans4").innerHTML = answers[3];
+}
+function answerhandler(answer) {
+    // answer should be saved for the evaluation at the end
+    alert(answer);
+    // generate new question
+    initQuestions();
+}
 //# sourceMappingURL=whoami.component.js.map
--- app/whoami.component.ts
+++ app/whoami.component.ts
@@ -7,5 +7,43 @@ import { Component, OnInit  } from '@angular/core';
 })
 export class WhoAmIComponent {
 
+
+    
+    startTimer() {
+        document.getElementById("gamewindow").innerHTML = "<div id ='delay'>GET READY...</div>";
+        setTimeout(initQuestions, 2000);
+    }
+
+    function initQuestions() {
+        //TODO: Generate Question
+        var question = 'What is the real name of Black Widow?';
+        //TODO: Fetch 4 answers from Watson
+        var answers = ['Peter Parker', 'Natalia Alianovna', 'Anthony Edward Stark', 'Wanda Maximoff']; 
+        
+        
+        document.getElementById("gamewindow").innerHTML = "<center><div id=question></div><br>\
+        <button class='button' id='ans1'>Answer 1</button>\
+        <button class='button' id='ans2'>Answer 2</button>\
+        <button class='button' id='ans3'>Answer 3</button>\
+        <button class='button' id='ans4'>Answer 4</button></center>";
+        document.getElementById("ans1").onclick=function(){answerhandler(1);};
+        document.getElementById("ans2").onclick=function(){answerhandler(2);};
+        document.getElementById("ans3").onclick=function(){answerhandler(3);};
+        document.getElementById("ans4").onclick=function(){answerhandler(4);};
+        
+        document.getElementById("question").innerHTML = question;
+        document.getElementById("ans1").innerHTML = answers[0];
+        document.getElementById("ans2").innerHTML = answers[1];
+        document.getElementById("ans3").innerHTML = answers[2];
+        document.getElementById("ans4").innerHTML = answers[3];
+    }
+    
+    function answerhandler(answer){
+        // answer should be saved for the evaluation at the end
+        alert(answer);
+        
+        // generate new question
+        initQuestions();
+    }
 }
 
